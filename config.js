527ebad72d260f1ee943979fa9e21412891e5933
sidey79 <7968127+sidey79@users.noreply.github.com>
2023-02-26 23:01:07 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
   "dryRun": false,
   "username": "sidey79-self-hosted-renovate[bot]",
   "gitAuthor": "Self-hosted Renovate Bot <123456+sidey-self-hosted-renovate[bot]@users.noreply.github.enterprise.com>",
-  "onboarding": false,
+  "onboarding": true,
   "platform": "github",
   "includeForks": true,
   "autodiscover": true,
